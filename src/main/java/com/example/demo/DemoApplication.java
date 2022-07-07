8d81a2f8d5550ab0a013881c604bdbc6d7226959
KeysGilbert <gbk5070@psu.edu>
2022-07-07 15:04:17 -0400

    make list of students

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,17 @@
 package com.example.demo;
 
+
+import com.example.demo.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+
 
 @SpringBootApplication
 @RestController
@@ -14,7 +22,9 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello, world!";
+	public List<Student> students() {
+		return List.of(
+				new Student(1L, LocalDate.of(1980, Month.JANUARY, 1), 30,"john@johndoe.com", "John Doe")
+		);
 	}
 }
